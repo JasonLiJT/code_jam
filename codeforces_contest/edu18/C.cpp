b4461ba0a9e5e2c66ad9d7a6cfafbaa4cda72080
JasonLiJT <JasonLiJT@users.noreply.github.com>
2017-03-28 18:05:43 +0100

    Topcoder practice begins

---
+++ codeforces_contest/edu18/C.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+#define endl '\n'
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n;
+	string s;
+	cin >> n;
+	for (int i = 0; i < n; ++i) {
+		/* code */
+	}
+}
