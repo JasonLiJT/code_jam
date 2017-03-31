a4c07e97c1013ea04db15839834e416152f9e151
JasonLiJT <JasonLiJT@users.noreply.github.com>
2017-03-31 01:03:46 +0100

    407d2 B solved

--- codeforces_contest/407d2/B.cpp
+++ codeforces_contest/407d2/B.cpp
@@ -4,11 +4,11 @@
 
 using namespace std;
 
-int b1, q, l, m, a[100009], b[30], count = 0;
+long long b1, q, l, m, a[100009], b, count = 0, upper = 35;
 bool inf = false;
 
-inline bool in_a(int x) {
-    for (int i = 0; i < m; ++i)
+inline bool in_a(long long x) {
+    for (long long i = 0; i < m; ++i)
     {
         if (x == a[i]) return true;
     }
@@ -17,14 +17,12 @@ inline bool in_a(int x) {
 
 int main() {
     
-    scanf("%d%d%d%d", &b1, &q, &l, &m);
-    b[0] = b1;
-    b[1] = b[0] * q;
-    for (int i = 0; i < m; ++i) {
-        scanf("%d", &a[i]);
+    scanf("%lld%lld%lld%lld", &b1, &q, &l, &m);
+    for (long long i = 0; i < m; ++i) {
+        scanf("%lld", &a[i]);
     }
 
-    if (q == -1 && (in_a(b1) && in_a(b1 * q)) || (abs(b1) > l)) {
+    if (q == -1 && ((in_a(b1) && in_a(b1 * q)) || (abs(b1) > l))) {
         printf("0\n");
         return 0;
     } else if (q == -1 && ((!in_a(b1) || !in_a(b1 * q)) && (abs(b1) <= l))) {
@@ -32,10 +30,13 @@ int main() {
         return 0;
     } else if (b1 * q == 0) {
         if (in_a(0)) {
-            printf("%d\n", (!in_a(b1)));
+            printf("%d\n", (!in_a(b1) && abs(b1) <= l));
+            return 0;
+        } else if (abs(b1) <= l) {
+            printf("inf\n");
             return 0;
         } else {
-            printf("inf\n");
+            printf("0\n");
             return 0;
         }
     } else if (q == 1) {
@@ -48,31 +49,19 @@ int main() {
         }
     }
 
-    m = 35;
-    while (in_a(b[0]) && (m + 1)) {
-        b[0] *= q;
-        m--;
-    }
-    if (m == -1) {
-        printf("0\n");
-        return 0;
-    }
-    count = 1;
-    for (int i = 0; i < 35; ++i) {
-        int next = b[i] * q;
-        // printf("next:%d\n", next);
-        if (next == 0 || abs(next) > 1000000000 || abs(next) > l) {
-            break;
+    // now abs(q) >= 2 && b1 != 0
+    b = b1;
+    count = 0;
+    while (count < 35 && abs(b) <= l) {
+        if (in_a(b)) {
+            b *= q;
+            continue;
         }
-        if (in_a(next)) continue;
         count++;
-        // printf("count++\n");
-        b[i+1] = next;
+        b *= q;
     }
 
-    printf("%d\n", count);
-    // for (int i = 0; i < count; ++i) {
-    //     printf("%d ", b[i]);
-    // }
+    if (count > 33) printf("inf\n");
+    else printf("%lld\n", count);
 
 }
