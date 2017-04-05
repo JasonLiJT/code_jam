57e9d8a45a9b8d98779493a4390e4f9750735087
JasonLiJT <JasonLiJT@users.noreply.github.com>
2017-04-06 00:35:34 +0100

    replace() in <algorithm>

--- practice/2009_Qualification/A/A-regex.cpp
+++ practice/2009_Qualification/A/A-regex.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <string>
-#include <set>
+#include <vector>
 #include <regex>
+#include <algorithm>
 
 using namespace std;
 #define endl '\n'
 
 int L, D, N;
-set<string> dict;
+vector<string> dict;
 
 int main() {
 	ios::sync_with_stdio(false);
@@ -16,21 +17,27 @@ int main() {
 	for (int i = 0; i < D; ++i) {
 		string s;
 		cin >> s;
-		dict.insert(s);
+		dict.push_back(s);
 	}
 	for (int X = 1; X < N + 1; ++X) {
 		int K = 0;
 		string line = "";
 		cin >> line;
-		for (int i = 0; i < L; ++i) {
-			int pos = line.find("(");
-			if (pos != -1) {
-				line[pos] = '[';
-				line[line.find(")")] = ']';
-			}
-		}
+
+		///////////////////////////////////////////////////
+		// for (int i = 0; i < L; ++i) {
+		// 	int pos = line.find("(");
+		// 	if (pos != -1) {
+		// 		line[pos] = '[';
+		// 		line[line.find(")")] = ']';
+		// 	}
+		// }
+		replace(line.begin(), line.end(), '(', '[');
+		replace(line.begin(), line.end(), ')', ']');
+		///////////////////////////////////////////////////
+
 		regex e(line);
-		for (std::set<string>::iterator i = dict.begin(); i != dict.end(); ++i) {
+		for (std::vector<string>::iterator i = dict.begin(); i != dict.end(); ++i) {
 			K += (regex_match(*i, e));
 		}
 		cout << "Case #" << X << ": " << K << endl;
