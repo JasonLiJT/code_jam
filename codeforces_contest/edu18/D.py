23df19b2ee323dff11c2430005bc848677b017fd
JasonLiJT <JasonLiJT@users.noreply.github.com>
2017-04-08 10:42:47 +0100

    edu18 D solved

--- codeforces_contest/edu18/D.py
+++ codeforces_contest/edu18/D.py
@@ -1,17 +1,12 @@
 from math import *
 
-n, q = [int(x) for x in input().split(' ')]
+n, q = list(map(int, input().split()))
 depth = int(log2(n + 1))
-
 
 def get_layer(u):
     for layer in range(depth):
         if u % (2 ** (layer + 1)) == 2 ** layer:
             return layer
-
-
-def is_right_child(layer, u):
-    return (u % 2 ** (layer + 1)) % 2 == 0
 
 
 for x in range(q):
@@ -21,20 +16,18 @@ for x in range(q):
     for s in steps:
         if s == 'U':
             if (layer == depth - 1):
-                print('Reached top!!!')
                 continue
             k = (u // (2 ** (layer + 1))) // 2
             layer += 1
             u = (2 ** (layer + 1)) * k + 2 ** layer
-            print('k = {}, layer = {}, u = {}'.format(k, layer, u))
         elif layer == 0:
             continue
         elif s == 'R':
-            layer -= 1
             k = (u // (2 ** (layer + 1))) * 2 + 1
+            layer -= 1
             u = (2 ** (layer + 1)) * k + 2 ** layer
         elif s == 'L':
+            k = (u // (2 ** (layer + 1))) * 2
             layer -= 1
-            k = (u // (2 ** (layer + 1))) * 2 + 1
             u = (2 ** (layer + 1)) * k + 2 ** layer
     print(u)
