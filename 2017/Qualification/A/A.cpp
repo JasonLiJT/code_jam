5f43c67a0af2b97a60ca382469b7b1c5b1e7fef1
JasonLiJT <JasonLiJT@users.noreply.github.com>
2017-04-09 10:58:46 +0100

    2017 Qualification A solved

--- 2017/Qualification/A/A.cpp
+++ 2017/Qualification/A/A.cpp
@@ -19,12 +19,12 @@ int main() {
 	cin.tie(NULL);
 
 	int T, K;
-	bool status[1000], current[1000];
+	bool status[1000];
 	string s;
 	cin >> T;
 	for (int i = 0; i < T; ++i) {
 		cin >> s >> K;
-		int ans  = 0, L = s.length(), width = L - K + 1, operations[1000] = {0};
+		int ans  = 0, L = s.length(), width = L - K + 1;
 		bool initial_done = true;
 		for (int c = 0; c < L; ++c) {
 			status[c] = (s[c] == '+') ? true : false;
@@ -36,38 +36,22 @@ int main() {
 		}
 		int j = 0;
 		for (; j < width; ++j) {
-			operations[width - 1 - j] = 1;
 			bool done = true;
-			do {
-				for (int x = 0; x < L; ++x) {
-					current[x] = status[x];
+			if (!status[j]) {
+				ans++;
+				for (int b = 0; b < K; ++b) {
+					status[j + b] = !status[j + b];
 				}
-				done = true;
-				ans = 0;
-				for (int a = 0; a < width; ++a) {
-					if (operations[a] == 1) {
-						ans++;
-						for (int b = 0; b < K; ++b) {
-							current[a + b] = !current[a + b];
-						}
-					}
+			}
+			for (int x = 0; x < L; ++x) {
+				if (!status[x]) {
+					done = false;
+					break;
 				}
-				for (int x = 0; x < L; ++x)
-				{
-					if (!current[x]) {
-						done = false;
-						break;
-					}
-				}
-				if (done) break;
-			} while (next_permutation(operations, operations + width));
+			}
 			if (done) break;
 		}
 
-		clog << endl;
-		arr_clog(status, L);
-		arr_clog(current, L);
-		arr_clog(operations, width);
 		if (j == width) {
 			cout << "Case #" << i + 1 << ": IMPOSSIBLE" << endl;
 			continue;
