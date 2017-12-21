ebff94a4c8e8bed9124d1fd77cf37a259e5aa7f6
JasonLiJT <JasonLiJT@users.noreply.github.com>
2017-12-21 21:23:17 +0000

    Random Contraction debugged. Screw remove()

--- practice/algo1/Random-contraction.cpp
+++ practice/algo1/Random-contraction.cpp
@@ -23,25 +23,21 @@ void print_graph(const unordered_map<int, vector<int> >& graph) {
     }
 }
 
-void random_contract(unordered_map<int, vector<int> >& graph, long long& edge_count2, long long& node_count) {
+void random_contract(unordered_map<int, vector<int> >& graph, long long& edge_count2) {
+    size_t node_count = graph.size();
     if (node_count < 3) {
         cout << "Reached 2 nodes!" << endl;
         return;
     }
 
-    cout << "edge_count2 = " << edge_count2 << ", node_count = " << node_count << " = " << graph.size() << endl;
-
     // Uniformly randomly select an edge
     long long r = rand() % edge_count2;
-    cout << "r: " << r << endl;
+    // cout << "r: " << r << endl;
     auto i = graph.begin();
 
-    int flag = 0;
     while (r >= i->second.size() && i != graph.end()) {
         r -= i->second.size();
         i++;
-        flag++;
-        // cout << "i+" << flag << "-" << r << " ";
     }
 
     if (i == graph.end()) {
@@ -50,21 +46,7 @@ void random_contract(unordered_map<int, vector<int> >& graph, long long& edge_co
     }
 
     // Edge selected, find nodes a, b
-    int a = i->first;
-    // auto b_iter = i->second.begin();
-    // while (r-- && b_iter != i->second.end()) {
-    // 	// BidirectionalIterator, no random access
-    // 	b_iter++;
-    // 	cout << "b+";
-    // }
-    // if (b_iter == i->second.end()) {
-    // 	cout << "Random number out of range at b_iter" << endl;
-    // 	cout << "i->second.size() = " << i->second.size() << endl;
-    // 	cout << "r = " << r << endl;
-    // 	return;
-    // }
-    // int b = *b_iter;
-    int b = *(i->second.begin() + r);
+    int a = i->first, b = *(i->second.begin() + r);
 
     // Iterators to the nodes to be contracted
     auto x = i, y = graph.find(b);
@@ -78,58 +60,35 @@ void random_contract(unordered_map<int, vector<int> >& graph, long long& edge_co
     int x2y = count(x->second.begin(), x->second.end(), y->first);
     int y2x = count(y->second.begin(), y->second.end(), x->first);
     if (x2y != y2x) {
-		print_graph(graph);
-        cout << "boom" << endl;
+        print_graph(graph);
     }
     edge_count2 -= x2y;
     edge_count2 -= y2x;
-    remove(x->second.begin(), x->second.end(), y->first);
-    remove(y->second.begin(), y->second.end(), x->first);
-
+    x->second.erase(remove(x->second.begin(), x->second.end(), y->first), x->second.end());
+    y->second.erase(remove(y->second.begin(), y->second.end(), x->first), y->second.end());
 
     // Redirect every edge with y to x
     try {
         for (auto y_node : y->second) {
-            cout << "y_node: " << y_node << " ";
-            int b_count = count(graph.at(y_node).begin(), graph.at(y_node).end(), b);  // at() provides bounds checking
-            remove(graph.at(y_node).begin(), graph.at(y_node).end(), b);
-            for (int i = 0; i < b_count; ++i) {
-                graph.at(y_node).push_back(a);
-                x->second.push_back(y_node);
+            x->second.push_back(y_node);
+            for (auto& i : graph.at(y_node)) {
+                if (i == b) {
+                    i = a;
+                }
             }
         }
     } catch (...) {
         cout << "\nErr caught!" << endl;
-        cout << "a = " << a << ", b = " << b << endl;
-        cout << "x: " << x->first << ": ";
-        for (const auto& j : x->second) {
-            cout << j << " ";
-        }
-        cout << endl;
-        cout << "y: " << y->first << ": ";
-        for (const auto& j : y->second) {
-            cout << j << " ";
-        }
-        cout << endl;
-        print_graph(graph);
-        cout << endl;
     }
 
     // Delete node y
-    if (b == 19) {
-        print_graph(graph);
-        cout << "a = " << a << ", b = " << b << endl;
-        cout << "WTF" << endl;
-    }
-    if (graph.erase(y) == graph.end()) {
-        cout << "Erasion failed for " << b << endl;
-    } else {
-        node_count--;
-    }
+    graph.erase(y);
 }
 
 int main() {
-    // srand(time(nullptr));
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    srand(time(nullptr));
 
     ifstream fin;
     fin.open("kargerMinCut.txt");
@@ -137,7 +96,7 @@ int main() {
     stringstream ss;
     unordered_map<int, vector<int> > graph;
 
-    long long edge_count2 = 0, node_count = 0;
+    long long edge_count2 = 0;
 
     // Read the graph
     while (getline(fin, line)) {
@@ -145,7 +104,6 @@ int main() {
         ss.str(line);
         int n, m;
         ss >> n;
-        node_count++;
         while (ss >> m) {
             edge_count2++;
             graph[n].push_back(m);
@@ -155,11 +113,22 @@ int main() {
 
     cout << "Read done" << endl;
 
-    while (node_count > 2) {
-        random_contract(graph, edge_count2, node_count);
-    }
+    size_t trials = 1, smallest_min = 200;
 
-    print_graph(graph);
+    while (trials--) {
+        unordered_map<int, vector<int> > tmp_graph(graph);
+        long long tmp_edge_count2 = edge_count2;
+        // srand(trials);
+        while (tmp_graph.size() > 2) {
+            random_contract(tmp_graph, tmp_edge_count2);
+        }
+
+        print_graph(tmp_graph);
+        if (tmp_graph.begin()->second.size() < smallest_min) {
+            smallest_min = tmp_graph.begin()->second.size();
+        }
+        cout << "\nMinimum cuts = " << smallest_min << endl;
+    }
 
     return 0;
 }
